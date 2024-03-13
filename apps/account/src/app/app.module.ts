512feadae5dcb9a65b2e34ec76e1579c7b299ce3
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-13 17:17:15 +0600

    added configuration and connection to the db

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './config/mongo.config';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'envs/.account.env',
+    }),
+    MongooseModule.forRootAsync(getMongoConfig()),
+    UserModule,
+    AuthModule
+  ],
 })
 export class AppModule {}
