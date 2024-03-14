5aac7284489186c14eb41b159c99b6d86e8703c0
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-14 11:13:59 +0600

    added rmq config

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getMongoConfig } from './config/mongo.config';
+import { RMQModule } from 'nestjs-rmq';
+import { getRMQConfig } from './config/rmq.config';
 
 @Module({
   imports: [
@@ -11,6 +13,7 @@ import { getMongoConfig } from './config/mongo.config';
       isGlobal: true,
       envFilePath: 'envs/.account.env',
     }),
+    RMQModule.forRootAsync(getRMQConfig()),
     MongooseModule.forRootAsync(getMongoConfig()),
     UserModule,
     AuthModule
