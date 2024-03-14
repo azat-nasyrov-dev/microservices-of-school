3215567d521b739d1a8ba831061655699b9f1f9f
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-14 22:51:52 +0600

    added logic to the command

--- apps/account/src/app/user/user.commands.ts
+++ apps/account/src/app/user/user.commands.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { UserRepository } from './repositories/user.repository';
+import { RMQRoute, RMQValidate } from 'nestjs-rmq';
+import { AccountChangeProfile } from '@microservices-of-school/contracts';
+import { UserEntity } from './entities/user.entity';
 
 @Controller()
-export class UserCommands {}
+export class UserCommands {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  @RMQValidate()
+  @RMQRoute(AccountChangeProfile.topic)
+  public async userInfo(@Body() { user, id }: AccountChangeProfile.Request): Promise<AccountChangeProfile.Response> {
+    const existedUser = await this.userRepository.findUserById(id);
+
+    if (!existedUser) {
+      throw new Error('Такого пользователя не существует');
+    }
+
+    const userEntity = new UserEntity(existedUser).updateProfile(user.displayName);
+    await this.userRepository.updateUser(userEntity);
+
+    return {};
+  }
+}
