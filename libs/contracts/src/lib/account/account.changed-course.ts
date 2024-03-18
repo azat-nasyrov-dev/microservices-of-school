f827565266a445f053a85f1ea15e7dbbed4b231f
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-18 18:30:31 +0600

    added a new contract and interface

---
+++ libs/contracts/src/lib/account/account.changed-course.ts
@@ -0,0 +1,17 @@
+import { PurchaseState } from '@microservices-of-school/interfaces';
+import { IsString } from 'class-validator';
+
+export namespace AccountChangedCourse {
+  export const topic = 'account.changed-course.event';
+
+  export class Request {
+    @IsString()
+    userId: string;
+
+    @IsString()
+    courseId: string;
+
+    @IsString()
+    state: PurchaseState;
+  }
+}
