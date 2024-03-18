d1700446e0b3b0090ea760a5346a2422475c2c02
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-18 18:30:59 +0600

    updated user entity

--- apps/account/src/app/user/entities/user.entity.ts
+++ apps/account/src/app/user/entities/user.entity.ts
@@ -1,5 +1,12 @@
-import { IUser, IUserCourses, PurchaseState, UserRole } from '@microservices-of-school/interfaces';
+import {
+  IDomainEvent,
+  IUser,
+  IUserCourses,
+  PurchaseState,
+  UserRole
+} from '@microservices-of-school/interfaces';
 import { compare, genSalt, hash } from 'bcryptjs';
+import { AccountChangedCourse } from '@microservices-of-school/contracts';
 
 export class UserEntity implements IUser {
   _id?: string;
@@ -8,6 +15,7 @@ export class UserEntity implements IUser {
   passwordHash: string;
   role: UserRole;
   courses?: IUserCourses[];
+  events: IDomainEvent[] = [];
 
   constructor(user: IUser) {
     this._id = user._id;
@@ -44,6 +52,10 @@ export class UserEntity implements IUser {
       return c;
     });
 
+    this.events.push({
+      topic: AccountChangedCourse.topic,
+      data: { courseId, userId: this._id, state }
+    });
     return this;
   }
 
